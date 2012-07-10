c4230081e065c223213a77af3964b9c95265cc4b
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-07-09 19:12:53 -0700

    Bump to 1.0a for config file fix

--- package.sh
+++ package.sh
@@ -6,11 +6,11 @@ rm -rf reobf/minecraft{,_server}/ic2
 
 pushd reobf/minecraft
 cp ../../port-berries++/client/*.png .
-zip -r ../../port-berries++/Berries++-1.0-client-r1.zip .
+zip -r ../../port-berries++/Berries++-1.0a-client-r1.zip .
 popd
 
 pushd reobf/minecraft_server
-zip -r ../../port-berries++/Berries++-1.0-server-r1.zip .
+zip -r ../../port-berries++/Berries++-1.0a-server-r1.zip .
 popd
 
 popd
