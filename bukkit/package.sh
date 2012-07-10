0a70deee89746579259329bf2d97f383c367ce6e
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-07-09 19:47:07 -0700

    Add mcmod.info

--- bukkit/package.sh
+++ bukkit/package.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p net/minecraft/server
 cp *.class net/minecraft/server
-zip -r Berries++-1.0a-mcpc1.2.5-r1.zip net/
+cp ../mcmod.info .
+zip -r Berries++-1.0a-mcpc1.2.5-r1.zip net/ mcmod.info
 
--- package.sh
+++ package.sh
@@ -5,11 +5,13 @@ pushd ..
 rm -rf reobf/minecraft{,_server}/ic2
 
 pushd reobf/minecraft
+cp ../../port-berries++/mcmod.info .
 cp ../../port-berries++/client/*.png .
 zip -r ../../port-berries++/Berries++-1.0a-client-r1.zip .
 popd
 
 pushd reobf/minecraft_server
+cp ../../port-berries++/mcmod.info .
 zip -r ../../port-berries++/Berries++-1.0a-server-r1.zip .
 popd
 
